28245ab9ddfb196b7bc5533336b2db83181efd1e
Sachin Kumar <Skumar@BHAVNACORP.NET>
2019-10-31 15:21:52 +0530

    Add button

--- src/compoents/Home/index.js
+++ src/compoents/Home/index.js
@@ -4,6 +4,10 @@ export default function Home() {
     return (
         <div>
             Home
+            <br></br>
+            <button >Add User</button>
+            <button >Delete User</button>
+            <button >View User List</button>
         </div>
     )
 }
